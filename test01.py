dcb30fb823100470529bdf341e987ea8323c4597
Sean <toranos@gmail.com>
2015-09-27 23:10:04 +1000

    Add test01 (test)

---
+++ test01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2.7 -u
+# test tests
+a = 10
+if not ((int(a) >= 0) and not (int(a) >= 1)):
+    print 'This is printed'
+else:
+    print 'This is not'
+if not (len('zero') == 0):
+    print 'This is printed'
+else:
+    print 'This is not'
+if not ((a == '0') and not (a == '1')):
+    print 'This is printed'
+else:
+    print 'This is not'
+if not (int(a) >= 0):
+    print 'This is not'
+else:
+    print 'This is printed'
