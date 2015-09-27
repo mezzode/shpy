8a63ec5002e79716f65986f3e76197439ad5e1e3
Sean <toranos@gmail.com>
2015-09-27 22:32:03 +1000

    test00.py update

--- test00.py
+++ test00.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python2.7 -u
 import sys
+# test nested expr
 print ((((int(sys.argv[1]) * 2) + 10) / 2) - int(sys.argv[1])) # number magic trick
-# should print 5 no matter what input
+# should print 5 given any int
